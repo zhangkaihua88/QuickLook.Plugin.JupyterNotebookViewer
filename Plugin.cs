c58b36a650a4393109d083a5c063912d8622b1ee
zhangkaihua88 <zhangkaihua@buaa.edu.cn>
2022-05-26 19:53:31 +0800

    v1

---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+using QuickLook.Common.Plugin;
+using QuickLook.Plugin.HtmlViewer;
+using UtfUnknown;
+
+namespace QuickLook.Plugin.JupyterNotebookViewer
+{
+    public class Plugin : IViewer
+    {
+        private WebpagePanel _panel;
+
+        public int Priority => 0;
+
+        public void Init()
+        {
+        }
+
+        public bool CanHandle(string path)
+        {
+            return !Directory.Exists(path) && new[] { ".ipynb" }.Any(path.ToLower().EndsWith);
+        }
+
+        public void Prepare(string path, ContextObject context)
+        {
+            context.PreferredSize = new Size(1000, 600);
+        }
+
+        public void View(string path, ContextObject context)
+        {
+            _panel = new WebpagePanel();
+            context.ViewerContent = _panel;
+            context.Title = Path.GetFileName(path);
+
+            _panel.NavigateToHtml(GenerateNotebookHtml(path));
+            _panel.Dispatcher.Invoke(() => { context.IsBusy = false; }, DispatcherPriority.Loaded);
+        }
+
+        public void Cleanup()
+        {
+            GC.SuppressFinalize(this);
+
+            _panel?.Dispose();
+            _panel = null;
+        }
+
+        private string GenerateNotebookHtml(string path)
+        {
+            var html = Resources.json2html;
+            html = html.Replace("{{prism_min_css}}", Resources.prism_min_css);
+            html = html.Replace("{{marked_min_js}}", Resources.marked_min_js);
+            html = html.Replace("{{prism_min_js}}", Resources.prism_min_js);
+            html = html.Replace("{{prism_min_css}}", Resources.prism_min_css);
+            html = html.Replace("{{prism_python_min_js}}", Resources.prism_python_min_js);
+            html = html.Replace("{{katex_min_css}}", Resources.katex_min_css);
+            html = html.Replace("{{katex_min_js}}", Resources.katex_min_js);
+            html = html.Replace("{{nbv_js}}", Resources.nbv_js);
+
+            var bytes = File.ReadAllBytes(path);
+            var encoding = CharsetDetector.DetectFromBytes(bytes).Detected?.Encoding ?? Encoding.Default;
+
+            var nbjson = encoding.GetString(bytes);
+            nbjson = WebUtility.HtmlEncode(nbjson);
+
+            html = html.Replace("{{content}}", nbjson);
+
+            return html;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # QuickLook.Plugin.JupyterNotebookViewer
+![](image/Screenshot.png)
+
+**Note**: *It will be slow, if the ipynb file is large (maybe 1M)*
+
+# Download and Installation
+1. Go to [Release page](https://github.com/QL-Win/QuickLook.Plugin.HelloWorld/releases) and download the latest version.
+2. Make sure that you have QuickLook running in the background. Go to your Download folder, and press Spacebar on the downloaded `.qlplugin` file.
+3. Click the "Install" button in the popup window.
+4. Restart QuickLook.
+5. Select the file(`.ipynb`) and press Spacebar.
+
+
+
+# Thanks to
+- [@Paddy Xu](https://github.com/xupefei)'s idea in [[feature request] Jupyter notebooks support](https://github.com/QL-Win/QuickLook/issues/462#issuecomment-570641376)
+- [QuickLook.Plugin.HtmlViewer](https://github.com/QL-Win/QuickLook/)
+- [nbviewer.js](https://github.com/kokes/nbviewer.js): Client side rendering of Jupyter notebooks
+- [UTF-unknown](https://github.com/CharsetDetector/UTF-unknown): charset detector build in C# - .NET standard 1.0 & .NET 4+
