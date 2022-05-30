ec808eba522483ec43afcef748b6068ba91894e3
zhangkaihua88 <zhangkaihua@buaa.edu.cn>
2022-05-30 11:27:45 +0800

    reduce plugin size

--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,6 +1,6 @@
 Remove-Item ..\QuickLook.Plugin.JupyterNotebookViewer.qlplugin -ErrorAction SilentlyContinue
 
-Copy-Item -Path ..\QuickLook.Plugin.HtmlViewer\runtimes\ -Destination ..\bin\Release\ -Recurse
+# Copy-Item -Path ..\QuickLook.Plugin.HtmlViewer\runtimes\ -Destination ..\bin\Release\ -Recurse
 $files = Get-ChildItem -Path ..\bin\Release\ -Exclude *.pdb,*.xml
 Compress-Archive $files ..\QuickLook.Plugin.JupyterNotebookViewer.zip
 Move-Item ..\QuickLook.Plugin.JupyterNotebookViewer.zip ..\QuickLook.Plugin.JupyterNotebookViewer.qlplugin
